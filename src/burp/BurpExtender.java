89bd4c5c76328176e8a19e92bd83079217a1a019
bit4 <bit4woo@163.com>
2018-10-19 15:19:33 +0800

    add function of knife to hackbar
    https://github.com/bit4woo/knife
    two function:
    1. Add host to scope
    2. Update cookie with latest cookie in history

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -11,6 +11,9 @@ import java.util.List;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
+import knife.AddHostToScopeMenu;
+import knife.UpdateCookieMenu;
+
 /**
  *
  * @author abdul.wahab
@@ -34,18 +37,6 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory {
         this.helpers = callbacks.getHelpers();
         this.stdout = new PrintWriter(callbacks.getStdout(), true);
         this.stderr = new PrintWriter(callbacks.getStderr(), true);
-        this.menu_list = new ArrayList();
-        this.Hack_Bar_Menu = new JMenu(this.MenuName);
-        this.Hack_Bar_Menu.add(new SQL_Menu(this));
-        this.Hack_Bar_Menu.add(new SQL_Error(this));
-        this.Hack_Bar_Menu.add(new SQli_LoginBypass(this));
-        this.Hack_Bar_Menu.add(new XSS_Menu(this));
-        this.Hack_Bar_Menu.add(new LFI_Menu(this));
-        this.Hack_Bar_Menu.add(new XXE_Menu(this));
-        this.Hack_Bar_Menu.add(new WebShell_Menu(this));
-        this.Hack_Bar_Menu.add(new Reverse_Shell_Menu(this));
-//        this.Hack_Bar_Menu.add(new Decoder_Encoder_Menu(this));
-        
         
         this.callbacks.setExtensionName(this.ExtensionName);
         this.callbacks.registerContextMenuFactory(this);
@@ -54,8 +45,28 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory {
     
     @Override
     public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
-        this.context = invocation;
-
+    	this.context = invocation;
+        
+        ArrayList<JMenuItem> menu_list = new ArrayList<JMenuItem>();
+        
+		byte context = invocation.getInvocationContext();
+		if (context == IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST) {
+			menu_list.add(new UpdateCookieMenu(this));
+		}
+        
+		menu_list.add(new AddHostToScopeMenu(this));
+        
+        JMenu Hack_Bar_Menu = new JMenu(this.MenuName);
+        Hack_Bar_Menu.add(new SQL_Menu(this));
+        Hack_Bar_Menu.add(new SQL_Error(this));
+        Hack_Bar_Menu.add(new SQli_LoginBypass(this));
+        Hack_Bar_Menu.add(new XSS_Menu(this));
+        Hack_Bar_Menu.add(new LFI_Menu(this));//learn from this
+        Hack_Bar_Menu.add(new XXE_Menu(this));
+        Hack_Bar_Menu.add(new WebShell_Menu(this));
+        Hack_Bar_Menu.add(new Reverse_Shell_Menu(this));
+        //Hack_Bar_Menu.add(new Decoder_Encoder_Menu(this));
+        
         menu_list.add(Hack_Bar_Menu);
         return menu_list;
     }
